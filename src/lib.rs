62ff516639b4f3a6b55edc9aab6c74a69dfa6e6c
Leonardo <leo@amini.ai>
2025-02-04 13:34:31 -0300

    Solved the permission issue, but let's not forget to document the stuff because we also need to authz to sign the messages

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
 use cosmwasm_std::{
     entry_point, to_json_binary, Binary, Deps, DepsMut, Empty, Env, MessageInfo, Response, StdResult,
-    CosmosMsg, AnyMsg, 
+    CosmosMsg, AnyMsg,
 };
 mod msg;
 mod state;
-use crate::msg::{ExecuteMsg, QueryMsg, CountResp, QueryResp, MsgOraclePushResult};
+use crate::msg::{ExecuteMsg, QueryMsg, CountResp, QueryResp, MsgOraclePushResult, MsgExec, Any};
 use crate::state::{initialize_counter, load_counter, increment_counter};
 use prost::Message; 
 
@@ -19,7 +19,7 @@ pub fn execute(deps: DepsMut, _env: Env, info: MessageInfo, msg: ExecuteMsg) -> 
     match msg {
         ExecuteMsg::Increment{} => execute_increment(deps),
         ExecuteMsg::PushOracleResult { job_id, results_json } => {
-            execute_push_oracle_result(job_id, results_json, info.sender.to_string())
+            execute_push_oracle_result(job_id, results_json, info.sender.to_string(), _env)
         }
     }
 }
@@ -32,23 +32,39 @@ fn execute_increment(deps: DepsMut) -> StdResult<Response> {
         .add_attribute("new_count", new_count.to_string()))
 }
 
-fn execute_push_oracle_result(job_id: u64, results_json: String, sender: String) -> StdResult<Response> {
+fn execute_push_oracle_result(job_id: u64, results_json: String, sender: String, env: Env) -> StdResult<Response> {
     let msg = MsgOraclePushResult {
-        creator: sender,
+        creator: sender.clone(),
         job_id,
         results_json,
     };
     
+    // Encode MsgOraclePushResult as Protobuf Any (for MsgExec)
     let mut buf = Vec::new();
-    msg.encode(&mut buf).unwrap(); // Encode to protobuf bytes
+    msg.encode(&mut buf).unwrap();
 
-    let msg = CosmosMsg::Any(AnyMsg {
+    let oracle_msg = Any {  // ✅ Use Protobuf Any for MsgExec
         type_url: "/aminichain.apigateway.MsgOraclePushResult".to_string(),
-        value: Binary::from(buf),
+        value: buf,
+    };
+
+    // Wrap inside MsgExec (Authorization Execution)
+    let exec_msg = MsgExec {
+        grantee: env.contract.address.to_string(),
+        msgs: vec![oracle_msg],  // ✅ Use Protobuf Any inside MsgExec
+    };
+    
+    let mut exec_buf = Vec::new();
+    exec_msg.encode(&mut exec_buf).unwrap();
+
+    // Convert MsgExec to CosmWasm AnyMsg for sending as CosmosMsg::Any
+    let msg_exec = CosmosMsg::Any(AnyMsg {  // ✅ Convert Protobuf Any to CosmWasm AnyMsg
+        type_url: "/cosmos.authz.v1beta1.MsgExec".to_string(),
+        value: Binary::from(exec_buf),
     });
 
     Ok(Response::new()
-        .add_message(msg)
+        .add_message(msg_exec)
         .add_attribute("action", "push_oracle_result")
         .add_attribute("job_id", job_id.to_string()))
 }
--- src/msg.rs
+++ src/msg.rs
@@ -37,3 +37,26 @@ pub struct MsgOraclePushResult {
     #[prost(string, tag = "3")]
     pub results_json: String,
 }
+
+/// MsgExec allows an account to execute messages using an authorization.
+/// https://github.com/cosmos/cosmos-sdk/blob/fe1c8af22ae3768ffa840c6caaf8ff0cb27149b5/x/authz/proto/cosmos/authz/v1beta1/tx.proto#L63
+#[derive(Message)]
+pub struct MsgExec {
+    /// The account executing the message (must have been granted authorization).
+    #[prost(string, tag = "1")]
+    pub grantee: String,
+
+    /// A list of messages to execute (wrapped in `Any` format).
+    #[prost(message, repeated, tag = "2")]
+    pub msgs: Vec<Any>,
+}
+
+/// `Any` represents a Cosmos SDK message with a type URL and binary payload.
+#[derive(Message)]
+pub struct Any {
+    #[prost(string, tag = "1")]
+    pub type_url: String,
+
+    #[prost(bytes, tag = "2")]
+    pub value: Vec<u8>,
+}
